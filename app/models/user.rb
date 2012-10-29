0def34fe40badbe29b44d2ca8d477b2f3b0e1b65
bpis <devendrab@blueplanetinfosolutions.com>
2012-10-29 18:23:27 +0530

    added extra information

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,24 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :oauth_expires_at, :oauth_token, :provider, :uid, :name, :sex, :about_me, :birthday, :hometown, :location, :relationships, :relationship_details, :publish_stream, :status_update, :photo_upload, :read_friendlists
   def self.from_omniauth(auth)
+  
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
       user.name = auth.info.name
       
-      user.sex = auth.info.gender
+      user.sex = auth.extra.raw_info.gender
       user.about_me = auth.info.about_me
-      user.birthday = auth.info.birthday
-      user.hometown = auth.info.hometown
-      user.location = auth.info.location
-      user.relationships = auth.info.relationships
-      user.relationship_details = auth.info.relationship_details
-      user.publish_stream = auth.info.publish_stream
-      user.status_update = auth.info.status_update
-      user.photo_upload = auth.info.photo_upload
-      user.read_friendlists = auth.info.read_friendlists
+      user.birthday = auth.extra.raw_info.birthday
+      user.hometown = auth.extra.raw_info.hometown.name
+      user.location = auth.extra.raw_info.location.name
+      user.relationships = auth.extra.raw_info.relationships
+      user.relationship_details = auth.extra.raw_info.relationship_status
+      user.publish_stream = auth.extra.raw_info.publish_stream
+      user.status_update = auth.extra.raw_info.status_update
+      user.image = auth.info.image
+      user.read_friendlists = auth.extra.raw_info.read_friendlists
       
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '153507064794513', 'f7df488637f466807a2d7b85b3ffb61e', :display => 'popup'
+  provider :facebook, '153507064794513', 'f7df488637f466807a2d7b85b3ffb61e', {:scope => 'email,offline_access,user_birthday,read_stream,user_about_me,user_hometown,user_location,user_relationships,user_relationship_details,publish_stream,user_status,read_friendlists', :display => 'popup'}
 end
 
